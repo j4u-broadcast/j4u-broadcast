e1308eca2f461714f533dedcdf021285932a37c2
chewmunk <chw1787@gmail.com>
2021-12-19 01:00:24 +0900

    Add files

---
+++ j4u/broadcast/takeover-documents/process/assets/j4ubroadcast.js
@@ -0,0 +1,7 @@
+$('head').append('<meta name="viewport" content="width=device-width, initial-scale=1.0" />');
+$('head').append('<link rel="stylesheet" href="./assets/j4ubroadcast.css">')
+$('body').removeClass($('body').attr('class'))
+$('table').removeClass($('table').attr('class'));
+$('body').addClass('j4u-body')
+$('table').addClass('j4u-table');
+``
